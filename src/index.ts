ffe5488a075fbcf488ddf502340a2235de2c8abf
unknown <DSelimov>
2024-05-20 16:43:35 +0300

    changes

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,18 @@
 import express, { Application, Request, Response } from 'express';
 import apiRouter from './api';
 import Users from './api/controllers/UserController';
+import cors from 'cors';
 
 // Create Express server
 const app: Application = express();
 
 // Define port
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 
 // Middleware
 app.use(express.json());
+
+app.use(cors());
 
 // Mount API routes
 app.use('/api', apiRouter);
