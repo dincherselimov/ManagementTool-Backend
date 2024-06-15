85dd845ea6731fa18ec096672a945700d09bfa15
dincherselimov <dinchitoo@gmail.com>
2024-06-15 18:11:16 +0300

    cors-for-production

--- src/index.ts
+++ src/index.ts
@@ -2,25 +2,19 @@ import express, { Application, Request, Response } from 'express';
 import apiRouter from './api';
 import Users from './api/controllers/UserController';
 
-// Create Express server
 const app: Application = express();
 
-// Define port
 const PORT = process.env.PORT || 3000;
 
 const cors = require('cors');
 
-app.use(cors({
-  origin: 'http://localhost:3001', // Frontend URL
-  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-  allowedHeaders: 'Content-Type,Authorization',
-}));
-// Middleware
+app.use(cors({ origin: 'http://localhost:3001',  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',allowedHeaders: 'Content-Type,Authorization',}));
+app.use(cors({ origin: 'https://management-tool-front.vercel.app/',  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',allowedHeaders: 'Content-Type,Authorization',}));
+
 app.use(express.json());
 
 app.use(cors());
 
-// Mount API routes
 app.use('/api', apiRouter);
 
 app.use('/api/users', Users);
