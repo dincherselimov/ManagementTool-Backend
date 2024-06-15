7f70b3169ad3597bf6bb9b5c4b1c1ea85537bf18
dincherselimov <dinchitoo@gmail.com>
2024-06-15 18:20:21 +0300

    updated-index-for-cors

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,26 @@
 import express, { Application, Request, Response } from 'express';
 import apiRouter from './api';
 import Users from './api/controllers/UserController';
+import cors from 'cors';
+
 
 const app: Application = express();
-
 const PORT = process.env.PORT || 3000;
 
-const cors = require('cors');
+// CORS configuration
+const corsOptions = {
+  origin: ['http://localhost:3001', 'https://management-tool-front.vercel.app'],
+  methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+};
 
-app.use(cors({ origin: 'http://localhost:3001',  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',allowedHeaders: 'Content-Type,Authorization',}));
-app.use(cors({ origin: 'https://management-tool-front.vercel.app/',  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',allowedHeaders: 'Content-Type,Authorization',}));
+app.use(cors(corsOptions));
 
 app.use(express.json());
 
-app.use(cors());
-
+// Routes
 app.use('/api', apiRouter);
-
 app.use('/api/users', Users);
-
 
 // Default route
 app.get('/', (req: Request, res: Response) => {
